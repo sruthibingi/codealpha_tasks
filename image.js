65cfb5e8e3171ec053011c702bb8d3cc23cbd298
sruthibingi <sruthibingi7@gmail.com>
2025-01-20 14:31:45 +0530

    Task 1: Image Gallery
    Created an image gallery where users can view a collection of images using HTML, CSS for layout and JavaScript for image navigation.

---
+++ image.js
@@ -0,0 +1,11 @@
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openfullImg(pic) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+function closefullImg() {
+    fullImgBox.style.display = "none";
+}
